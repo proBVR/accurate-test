ec29f3ca569939fdc6c1828c7a29bea85c30aa34
akutoyuuki <akutoyuuki@gmail.com>
2018-10-15 17:02:01 +0900

    accrate test

---
+++ Assets/MainContoroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainContoroller : MonoBehaviour {
+    public UDPReceiver updReceiver;
+    public Transform phoneTf;
+    public Quaternion phoneRot;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPReceiver.AccelCallBack += AccelAction;
+        UDPReceiver.GyroCallBack += GyroAction;
+        updReceiver.UDPStart();
+    }
+
+    public void AccelAction(float xx, float yy, float zz)
+    {
+
+    }
+
+    public void GyroAction(float xx, float yy, float zz, float ww)
+    {
+        var newQut = new Quaternion(-xx, -zz, -yy, ww);
+        var newRot = newQut * Quaternion.Euler(90f, 0, 0);
+        phoneRot = newRot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phoneTf.localRotation = phoneRot;
+    }
+}
---
+++ Assets/UDPReceiver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+public class UDPReceiver : MonoBehaviour
+{
+    int LOCAL_PORT = 22222;
+    static UdpClient udp;
+    Thread thread;
+
+    public static Action<float, float, float> AccelCallBack;
+    public static Action<float, float, float, float> GyroCallBack;
+
+    public void UDPStart()
+    {
+        udp = new UdpClient(LOCAL_PORT);
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    private static void ThreadMethod()
+    {
+        while (true)
+        {
+            IPEndPoint remoteEp = null;
+            byte[] data = udp.Receive(ref remoteEp);
+            string text = Encoding.ASCII.GetString(data);
+
+            JsonNode jsonNode = JsonNode.Parse(text);
+
+            double ax = jsonNode["sensordata"]["accel"]["x"].Get<double>();
+            double ay = jsonNode["sensordata"]["accel"]["y"].Get<double>();
+            double az = jsonNode["sensordata"]["accel"]["z"].Get<double>();
+
+            double qutX = jsonNode["sensordata"]["quaternion"]["x"].Get<double>();
+            double qutY = jsonNode["sensordata"]["quaternion"]["y"].Get<double>();
+            double qutZ = jsonNode["sensordata"]["quaternion"]["z"].Get<double>();
+            double qutW = jsonNode["sensordata"]["quaternion"]["w"].Get<double>();
+
+            AccelCallBack((float)ax, (float)ay, (float)az);
+            GyroCallBack((float)qutX, (float)qutY, (float)qutZ, (float)qutW);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        thread.Abort();
+    }
+}
